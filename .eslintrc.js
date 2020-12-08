d16228575e11c2352c764d808c8b74b95d041c0e
denis.fominykh <denis.fominykh@yalantis.net>
2020-12-08 12:04:56 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import './App.scss';
+
+const App: FC = () => {
+  return (
+    <div className="app">
+      <h1>Hello App</h1>
+    </div>
+  );
+};
+
+export default App;
