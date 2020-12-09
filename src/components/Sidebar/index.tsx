4665805ef3530b3cdb98f9aeda96faa929b691e6
denis.fominykh <denis.fominykh@yalantis.net>
2020-12-09 12:27:28 +0200

    creating a sidebar component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import './index.scss';
+
+const Sidebar: FC = () => {
+  return (
+    <>
+      <nav className="sidebar">Navigation</nav>
+    </>
+  );
+};
+
+export default Sidebar;
