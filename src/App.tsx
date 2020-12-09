269ad342c6b77ee3ac9d0ed1f0d1e7707bac53d4
denis.fominykh <denis.fominykh@yalantis.net>
2020-12-09 13:12:49 +0200

    adding components to the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import React, { FC } from 'react';
 
 import './App.scss';
 
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Main from './containers/Main';
+
 const App: FC = () => {
   return (
-    <div className="app">
-      <h1>Hello App</h1>
+    <div className="container">
+      <Header />
+      <div className="content">
+        <Sidebar />
+        <Main />
+      </div>
     </div>
   );
 };
