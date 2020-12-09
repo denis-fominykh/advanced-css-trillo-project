5ac50d24721f9d4ea848840c01f6ce5653543b52
denis.fominykh <denis.fominykh@yalantis.net>
2020-12-09 12:25:26 +0200

    creating a main container

---
+++ src/containers/Main/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import './index.scss';
+
+const Main: FC = () => {
+  return (
+    <>
+      <main className="hotel-view">Main</main>
+    </>
+  );
+};
+
+export default Main;
